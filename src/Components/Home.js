065d788d9f09608e7d6aca0832b704b7047ad0cc
shirishSIB <shirish.bajpai@sendinblue.com>
2021-09-13 17:58:23 +0530

    updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,35 +2,34 @@ import './Home.css';
 import { useState } from 'react';
 
 const Home = () => {
-    const initialValue = [];
-    const [value, setValue] = useState([]);
+    const [values, setValues] = useState([]);
     const [initial, setInitial] = useState(false);
     var val = '';
     const onChange = (e) => {
         val = e.target.value;
-        if (initial == true) {
+        if (initial) {
             e.target.value = "";
             setInitial(false);
         }
     }
     const onClickHandler = (e) => {
-        if (val != "") {
-            setValue([...value, val]);
+        if (val !== "") {
+            setValues([...values, val]);
             //console.log(e.target.value, "val");
             setInitial(true);
         }
     }
     const onComplete = (e) => {
         //console.log(e.target.innerText);
-        const arr = value.filter((item) => item !== e.target.innerText);
-        setValue(arr);
+        const arr = values.filter((item) => item !== e.target.innerText);
+        setValues(arr);
     }
     return (
         <div className="todo">
             <h1>Todo-List</h1>
             <input className="inputVal" type="text" onChange={onChange} placeholder="Enter the Task...." /> &nbsp;
             <button className="btn" onClick={onClickHandler}>Add Task</button><br/><br/>
-            {value.map((user) => (
+            {values.map((user) => (
                 <div className="todo-task" onClick={onComplete} >{user}</div>
             ))}
         </div>
