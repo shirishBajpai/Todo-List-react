a2f05046759f4023437e81d29832a88a0daf77a4
shirishSIB <shirish.bajpai@sendinblue.com>
2021-09-15 12:47:58 +0530

    home-updated-2

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,48 +2,41 @@ import './Home.css';
 import { useState } from 'react';
 
 const Home = () => {
-    const [values, setValues] = useState([]);
-    //const [initial, setInitial] = useState(false);
+    const [todo, setTodo] = useState([]);
     const [change, setChange] = useState('');
     const onChange = (e) => {
         const val = e.target.value;
-        //if (initial) {
-         //  e.target.value = "";
-         //  setInitial(false);
-//}
         setChange(val);
-        console.log(val);
     }
 
     const taskLeft = () => {
-        return values.length;
+        return todo.length;
     }
 
-    const onClickHandler = (e) => {
+    const addTask = (e) => {
         if (change !== "") {
             const timestamp = Date.now();
-            setValues([...values, { name: change, id: timestamp }]);
-            //setInitial(true);
+            setTodo([...todo, { name: change, id: timestamp }]);
             console.log("dd");
             setChange('');
         }
     }
 
     const onComplete = (e) => {
-        const arr = values.filter((item) => item.id != e.target.id);
-        setValues(arr);
+        const arr = todo.filter((item) => item.id != e.target.id);
+        setTodo(arr);
     }
 
     const cancelTasks = () => {
-        setValues([]);
+        setTodo([]);
     }
 
     return (
         <div className="todo">
             <h1>Todo-List</h1>
-            <input className="todo_input" type="text" onChange={onChange} value={change} /> &nbsp;
-            <button className="todo_btn_addTask" onClick={onClickHandler}>Add Task</button><br /><br />
-            {values.map((user) => (
+            <input className="todo_input" type="text" onChange={onChange} value={change} placeholder="Enter your task...."/> &nbsp;
+            <button className="todo_btn_addTask" onClick={addTask}>Add Task</button><br /><br />
+            {todo.map((user) => (
                 <div className="todo_task" onClick={onComplete} key={user.id} id={user.id}>{user.name}</div>
             ))}
             <br />
