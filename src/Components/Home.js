c53c974332d37c49fb48e2844056355e0dbb95b9
shirishSIB <shirish.bajpai@sendinblue.com>
2021-09-15 12:32:26 +0530

    home-updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,36 +3,52 @@ import { useState } from 'react';
 
 const Home = () => {
     const [values, setValues] = useState([]);
-    const [initial, setInitial] = useState(false);
-    var val = '';
+    //const [initial, setInitial] = useState(false);
+    const [change, setChange] = useState('');
     const onChange = (e) => {
-        val = e.target.value;
-        if (initial) {
-            e.target.value = "";
-            setInitial(false);
-        }
+        const val = e.target.value;
+        //if (initial) {
+         //  e.target.value = "";
+         //  setInitial(false);
+//}
+        setChange(val);
+        console.log(val);
     }
+
+    const taskLeft = () => {
+        return values.length;
+    }
+
     const onClickHandler = (e) => {
-        if (val !== "") {
-            setValues([...values, val]);
-            //console.log(e.target.value, "val");
-            setInitial(true);
+        if (change !== "") {
+            const timestamp = Date.now();
+            setValues([...values, { name: change, id: timestamp }]);
+            //setInitial(true);
+            console.log("dd");
+            setChange('');
         }
     }
+
     const onComplete = (e) => {
-        console.log("f");
-        //console.log(e.target.innerText);
-        const arr = values.filter((item) => item !== e.target.innerText);
+        const arr = values.filter((item) => item.id != e.target.id);
         setValues(arr);
     }
+
+    const cancelTasks = () => {
+        setValues([]);
+    }
+
     return (
         <div className="todo">
             <h1>Todo-List</h1>
-            <input className="inputVal" type="text" onChange={onChange} placeholder="Enter the Task...." /> &nbsp;
-            <button className="btn" onClick={onClickHandler}>Add Task</button><br/><br/>
+            <input className="todo_input" type="text" onChange={onChange} value={change} /> &nbsp;
+            <button className="todo_btn_addTask" onClick={onClickHandler}>Add Task</button><br /><br />
             {values.map((user) => (
-                <div className="todo-task" onClick={onComplete} >{user}</div>
+                <div className="todo_task" onClick={onComplete} key={user.id} id={user.id}>{user.name}</div>
             ))}
+            <br />
+            Task left :-<span> {taskLeft()}</span>
+            <button className="todo_btn_cancelTasks" onClick={cancelTasks}>Cancel All Tasks</button><br /><br />
         </div>
     );
 }
